aa27d786871eb3404d1ff08fd2663dc73c0f6444
OanaSurdea <oana.surdea@ness.com>
2023-03-12 13:15:53 +0200

    1 file modified

--- src/app/music-player/components/_controls/volume-controls/volume-controls.component.ts
+++ src/app/music-player/components/_controls/volume-controls/volume-controls.component.ts
@@ -1,9 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-volume-controls',
   templateUrl: './volume-controls.component.html',
   styleUrls: ['./volume-controls.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VolumeControlsComponent {
   @Input() readonly isMuted: boolean = false;
