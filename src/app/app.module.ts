196970d666eae40f27e5dba7e58353d488cc594c
OanaSurdea <73647486+OanaSurdea@users.noreply.github.com>
2023-03-14 18:56:13 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MusicPlayerModule } from './music-player/music-player.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MusicPlayerModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MusicPlayerModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
